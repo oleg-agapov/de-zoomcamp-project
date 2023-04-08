01973040e3194c823f85f96e04c16f4b75da917d
Oleg Agapov <oleg.agapov.ua@gmail.com>
2023-04-08 20:15:53 +0200

    Fix dim_repositories

--- dbt/models/marts/dim_repositories.sql
+++ dbt/models/marts/dim_repositories.sql
@@ -1,27 +1,30 @@
 {{
     config(
         materialized = "incremental",
-        unique_key="repo_id"
+        unique_key=["repo_id", "repo_name"],
     )
 }}
 
+
 {% if is_incremental() %}
-  {%- call statement('max_load_time', fetch_result=True) -%}
+    {%- call statement('max_load_time', fetch_result=True) -%}
 
-    select max(load_time) from {{ this }}
+        select max(load_time) from {{ this }}
 
-  {%- endcall -%}
+    {%- endcall -%}
 
-  {%- set prev_load_time = load_result('max_load_time')['data'][0][0] -%}
+    {%- set prev_load_time = load_result('max_load_time')['data'][0][0] -%}
 {% endif %}
 
 
-select distinct
+select 
     repo_id,
-	  repo_name,
-    max(created_at) over() as load_time
+	repo_name,
+    max(created_at) as load_time
 from {{ ref('stg_github__events') }}
 
 {% if is_incremental() %}
 where created_at >= timestamp('{{ prev_load_time }}')
 {% endif %}
+
+group by 1, 2
